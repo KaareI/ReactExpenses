618a3be18c274bbe6d6b1319fd99279e45c9a0b7
Kaarel <karlkaareltaniel.seppel@gmail.com>
2023-01-19 13:55:22 +0200

    9. Vormitöötlus Reactis

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import ExpenseItem from "./components/Expenses/ExpenseItem";
 import ExpenseDate from "./components/Expenses/ExpenseDate";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () =>{
     const expenses = [
@@ -16,8 +17,14 @@ const App = () =>{
             price: 99.99
         }
     ]
+
+    const addExpensehandler = (expense) => {
+        console.log('In app.js')
+        console.log(expense)
+    }
     return (
         <div className="App">
+            <NewExpense onAddExpense={addExpensehandler}></NewExpense>
             <Expenses expenseData={expenses}></Expenses>
         </div>
     );
