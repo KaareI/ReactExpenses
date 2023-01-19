10ef7233a6c99fb088eea76ca76449c0957be684
Kaarel <karlkaareltaniel.seppel@gmail.com>
2023-01-19 11:02:23 +0200

    5. Ülesanne 2 - ümbritseva komponendi loomine ja kujundamine(Kõige õige)

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,3 +1,4 @@
+import './ExpenseDate.css'
 function ExpenseDate(props) {
     const day = props.date.toLocaleString('en-US', {day: '2-digit'})
     const month = props.date.toLocaleString('en-US', {month: 'long'})
