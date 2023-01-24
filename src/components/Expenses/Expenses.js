841217987c7402a41e3a2f3814701e8bb775e597
Kaarel <karlkaareltaniel.seppel@gmail.com>
2023-01-24 11:39:53 +0200

    10. Ülesanne 3 - sisekomponendi andmete suunamine välikomponendile

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,21 @@
 import './Expenses.css'
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import {useState} from "react";
 
 const Expenses = (props) =>{
+    const[filteredYear, setFilteredYear]=
+    useState(2023)
+    console.log('Year data in Expense.js ' + filteredYear)
+
+    const filterChangeHandler = (year) =>{
+        console.log('Filter change handled by Expenses.js')
+        setFilteredYear(year)
+    }
     return(
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
             <ExpenseItem expenseData={props.expenseData[0]}></ExpenseItem>
             <ExpenseItem expenseData={props.expenseData[1]}></ExpenseItem>
         </Card>
